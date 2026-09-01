51a04f6df5df9ce4db6e7c54b16aca4ce2fee6ef
Rava Ganteng katanya <isancokk@gmail.com>
2026-09-01 16:19:48 +0700

    database

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  TELEGRAM_TOKEN: "8515074204:AAGekvumcKGt_tiow5bMUBM-7ZEbrqxdBZw",
+  TELEGRAM_CREATOR: ["7462658464", "6278299155"],
+  
+  GITHUB_TOKEN: "https://raw.githubusercontent.com/ravadeveloper/database/refs/heads/main/necro.json",
+  GITHUB_OWNER: "ravadeveloper",
+  GITHUB_REPO: "database",
+  GITHUB_PATH: "necro",
+  ROLES_FILE: "./database/necro.json",
+}
